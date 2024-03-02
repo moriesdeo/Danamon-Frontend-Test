ae7ad66f9456fc4b1b68ffdf157972079c7d2ce7
Agresia Rettha <54567254+Agresiarthtp@users.noreply.github.com>
2024-03-03 00:40:49 +0700

    first commit

---
+++ core/src/main/java/com/danamon/core/extension/FlowExt.kt
@@ -0,0 +1,21 @@
+package com.danamon.core.extension
+
+import com.danamon.core.data.Resource
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.flowOn
+import kotlinx.coroutines.flow.onCompletion
+import kotlinx.coroutines.flow.onStart
+
+fun <T> Flow<Resource<T>>.buildNetwork() =
+    onStart {
+        emit(Resource.Loading(true))
+    }.onCompletion {
+        emit(Resource.Loading(false))
+    }.
+    catch { error ->
+        emit(Resource.Error(Throwable(message = error.message)))
+        emit(Resource.Loading(false))
+    }
+        .flowOn(Dispatchers.IO)
